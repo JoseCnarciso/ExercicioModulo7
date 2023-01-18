fbf7072970dd26058a595980a3ee562c3801df20
jose carlos narciso <josecdia@hotmail.com>
2023-01-17 21:40:23 -0300

    Initial commit

---
+++ api/src/main/java/com/apihospitalar/api/ApiHospitalarApplication.java
@@ -0,0 +1,13 @@
+package com.apihospitalar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHospitalarApplication.class, args);
+	}
+
+}
